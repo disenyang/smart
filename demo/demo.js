a994a39849c484c70f7a689dea6ee12cb6581758
bboy_xiaoyang <shugao19880814@163.com>
2015-11-03 19:26:58 +0800

    文件目录修改

--- demo/demo.js
+++ demo/demo.js
@@ -1,13 +1,13 @@
 $$.controller=function(){
 	$$.show = "1111";
 	$$.username = "bboy_xiaoyang";
-	$$.rows =[{name:"高扬",sex:0},{name:"张涛",sex:0}];
+	$$.rows =[{name:"将易于",sex:0},{name:"张涛",sex:0}];
 	$$.changeValue=function(){
 	    $$.show = "11";
 	    console.log($$.show);
 	    $$.show = "11";
 	    $$.username = "呵呵呵呵";
-	    $$.rows =[{name:"高扬11",sex:"男2"},{name:"张涛222",sex:"女2"}];
+	    $$.rows =[{name:"将易于22",sex:"男2"},{name:"张涛222",sex:"女2"}];
 	}
 };
 
